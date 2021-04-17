54fe1908b51f57d70da920a39aa499159a74de7b
Brian Viana <brianmviana@gmail.com>
2021-04-16 23:34:40 -0300

    Creating the Event model

---
+++ src/main/java/dev/brianmviana/events/models/Event.java
@@ -0,0 +1,72 @@
+package dev.brianmviana.events.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.data.annotation.Id;
+
+@Entity
+public class Event {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank
+	private String name;
+	
+	
+	@NotBlank
+	private String local;
+	
+	@NotBlank
+	private String date;
+	
+	@NotBlank
+	private String eventTime;
+	
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getLocal() {
+		return local;
+	}
+	
+	public void setLocal(String local) {
+		this.local = local;
+	}
+	
+	public String getDate() {
+		return date;
+	}
+	
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	public String getEventTime() {
+		return eventTime;
+	}
+	
+	public void setEventTime(String eventTime) {
+		this.eventTime = eventTime;
+	}
+	
+	
+}
